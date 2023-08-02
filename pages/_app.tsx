2ef81981b1a34130af7c5a84d5fe6bf9526a6fde
MPerez <marioxd99@gmail.com>
2023-08-02 20:15:31 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
+  )
 }
